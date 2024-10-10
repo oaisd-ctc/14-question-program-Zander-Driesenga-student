560d44d8f09d0a08d5d3a30b2ad7774b8ec6d703
Zander-Driesenga-student <zadr09b@student.oaisd.org>
2024-10-10 23:56:14 +0000

    finished the lab

--- 14_qustions.cs
+++ 14_qustions.cs
@@ -6,6 +6,7 @@ public class Program
         Console.WriteLine("What is your friends name?");
         string name = Console.ReadLine();
         Console.WriteLine("What is their age?");
+        
         int age = int.Parse(Console.ReadLine());
         Console.WriteLine("What is the color of their hair?");
         string hairColor = Console.ReadLine();
@@ -15,6 +16,18 @@ public class Program
         string favColor = Console.ReadLine();
         Console.WriteLine("What is their favorite tv show or movie");
         string favMovieOrTv = Console.ReadLine();
-        Console.WriteLine("Your friends name is " + name + age + hairColor + shoes + favColor);
+        Console.WriteLine("Who is their favorite teacher");
+        string favTeach = Console.ReadLine();
+        Console.WriteLine("What is their class");
+        string favClass = Console.ReadLine();
+        Console.WriteLine("What is their favorite holiday");
+        string favHoliday = Console.ReadLine();
+        Console.WriteLine("What is their favorite season");
+        string favSeason = Console.ReadLine();
+        Console.WriteLine("What is their dream job");
+        string dreamJob = Console.ReadLine();
+        Console.WriteLine("How many siblings do they have");
+        string siblings = Console.ReadLine();
+        Console.WriteLine("Your friends name is " + name + " they are " + age + " years old their hair color is " + hairColor + " they normaly wear " + shoes + " shoes " + " their favorite color is " + favColor + " their favorite tv show/movie is " + favMovieOrTv + " their favorite teacher is " + favTeach + " their favorite class is " + favClass + " their favorite holiday is " + favHoliday + " their favorite season is " + favSeason + " their dream job is " + dreamJob + " they have " + siblings + " they will be " + (age + 5) + " in 5 years");
     }
 }
